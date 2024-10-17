5b2956a57074ae3d0f0f0fdcca896e0a26743523
xandone <765478955@qq.com>
2024-10-17 17:56:39 +0800

    init

--- lib/ui/52im/im_list_page.dart
+++ lib/ui/52im/im_list_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:gbk_codec/gbk_codec.dart';
 import 'package:html/dom.dart' as dom;
 import 'package:html/parser.dart' show parse;
+import 'package:litenews/res/colors.dart';
 
 import '../../http/api.dart';
 import '../../utils/logger.dart';
@@ -45,6 +46,7 @@ class ImListState extends State<ImListPage> {
 
     setState(() {
       list?.forEach((it) {
+        datas.clear();
         Log.d(it.text);
         datas.add(it.text);
       });
@@ -54,7 +56,31 @@ class ImListState extends State<ImListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('datas[0]'),
+      body: ListView.builder(
+        itemCount: datas.length,
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            child: Card(
+              margin: const EdgeInsets.fromLTRB(12, 8, 12, 0),
+              child: Padding(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      datas[index],
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(fontSize: 14, color: MyColors.b1_color),
+                    ),
+                    Text('data'),
+                  ],
+                ),
+                padding: EdgeInsets.all(12),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
